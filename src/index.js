9775bfac804355587db1364b5d0e0e53a0e157e4
anderson <anderson.d.cardoso92@gmail.com>
2020-11-30 22:00:37 +0400

    adding list elements to display component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
+const list_items = [
+  "Item 1",
+  "Item 2",
+  "Item 3",
+  "Item 4",
+  "Item 5",
+  "Item 6",
+  "Item 7",
+  "Item 8",
+  "Item 9",
+  "Item 10",
+  "Item 11",
+  "Item 12",
+  "Item 13",
+  "Item 14",
+  "Item 15",
+  "Item 16",
+  "Item 17",
+];
 
+const list_element = document.getElementById("list");
+const pagination_element = document.getElementById("pagination");
 
-console.log(fetch);
+let current_page = 3;
+let rows = 5;
+
+function DispayList(items, wrapper, rows_per_page, page) {
+  wrapper.innerHTML = "this is the list element";
+  page--;
+
+  let start = rows_per_page * page;
+  let end = start + rows_per_page;
+  let items_in_page = items.slice(start, end);
+
+  for (let i = 0; i < items_in_page.length; i++) {
+    let item = items_in_page[i];
+
+    let item_element = document.createElement("div");
+    item_element.classList.add("item")
+  }
+}
+
+// test function
+DispayList(list_items, list_element, 5, current_page);
